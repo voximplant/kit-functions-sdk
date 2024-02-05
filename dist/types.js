fe602a87d656aa4d263b13471e5979366d4912f6
dbuzinin <dbuzinin@voximplant.com>
2024-02-05 17:30:20 +0300

    feat: Added setReplyWebChatInlineButtons method

--- dist/types.js
+++ dist/types.js
@@ -1,2 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.WebChatInlineButtonType = void 0;
+var WebChatInlineButtonType;
+(function (WebChatInlineButtonType) {
+    WebChatInlineButtonType["Text"] = "text";
+})(WebChatInlineButtonType = exports.WebChatInlineButtonType || (exports.WebChatInlineButtonType = {}));
